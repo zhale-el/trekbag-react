729aaee52d5b407148495ed22a151a6bfff3a46b
zhaleh <zhalehelmi@gmail.com>
2024-10-30 12:52:24 +0330

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import BackgroundHeading from "./components/BackgroundHeading";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <BackgroundHeading />
+
+      <main></main>
+
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      <small>&copy; 2050. Copyright by Zhale. </small>
+      <p>
+        Version <b>1.5</b>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
