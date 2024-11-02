6b62ec7aa7f2e6e5f8fbfdf9761f17f9c46e6625
zhaleh <zhalehelmi@gmail.com>
2024-11-02 12:57:40 +0330

    itemlist

--- src/components/ButtonGrop.jsx
+++ src/components/ButtonGrop.jsx
@@ -1,13 +1,7 @@
+import { secondaryButtons } from "../lib/constants";
 import Button from "./Button";
 
 const ButtonGrop = () => {
-  const secondaryButtons = [
-    "Mark all as complete",
-    "Mark all as incomplete",
-    "Reset to initial",
-    "Remove all items",
-  ];
-
   return (
     <section className="button-group">
       {secondaryButtons.map((text) => {
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import { initialItems } from "../lib/constants";
+
 const ItemList = () => {
-    return (
-        <div>
-            Enter
-        </div>
-    );
-}
+  const [items, setItems] = useState(initialItems);
+
+  return (
+    <ul>
+      {items.map((item) => {
+        return <Item key={item.id} item={item} />;
+      })}
+    </ul>
+  );
+};
 
 export default ItemList;
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <label>
+        <input checked={item.packed} type="checkbox" />
+        {item.name}
+      </label>
+      <button>❌</button>
+    </li>
+  );
+}
